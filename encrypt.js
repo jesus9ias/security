fea01e77fef01a1c52c05c35b0869dcc4a322759
Jesús Escamilla <jesusescamilla@hotmail.com>
2015-10-13 22:39:35 -0500

    add bip38 example

--- encrypt.js
+++
@@ -1,8 +0,0 @@
-var Bip38 = require('bip38');
-
-var privateKeyWif = '5KN7MzqK5wt2TP1fQCYyHBtDrXdJuXbUzm4A9rKAteGu3Qi5CVR';
-
-var bip38 = new Bip38();
-bip38.version = {private: 0x80, public: 0x0};
-var enc = bip38.encrypt(privateKeyWif, "super-secret", "1Jq6MksXQVWzrznvZzxkV6oY57oWXD9TXB");
-console.log(enc);
